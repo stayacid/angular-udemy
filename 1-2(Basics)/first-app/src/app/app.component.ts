1176553d01e6362d71151c4063f206c9e4ce0e15
stayacid <stayacid@gmail.com>
2020-06-25 12:25:57 +0300

    learning basic

--- 1-2(Basics)/first-app/src/app/app.component.ts
+++ 1-2(Basics)/first-app/src/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  // or styles: [`h3 { color: blue}`]
 })
 export class AppComponent {
   name = 'first-app';
---
+++ 1-2(Basics)/first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  // or template: '<app-server></app-server><app-server></app-server>'
+  styleUrls: ['./servers.component.scss']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
